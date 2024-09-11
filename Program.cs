01e586badc97d9d5d2a98739f6cc99f732471ac2
Ali <alifaki077@gmail.com>
2024-09-11 22:23:26 +0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿using Telegram.Bot;
+
+string API_TOKEN = Environment.GetEnvironmentVariable("BUU_DUYURU_BOT_API_TOKEN") ?? "";
+var bot = new TelegramBotClient(API_TOKEN);
+var me = await bot.GetMeAsync();
+Console.WriteLine($"Hello, World! I am user {me.Id} and my name is {me.FirstName}.");
---
+++ README.md
@@ -0,0 +1,1 @@
+# BUÜ Duyuru Bot
